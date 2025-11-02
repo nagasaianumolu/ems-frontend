05d9f9e41bfb5699cc166b2c7ca11d6fc50eabc5
nagasaianumolu <nagasaianumolu@gmail.com>
2025-11-02 23:58:15 +0530

    add deployment code

---
+++ .history/src/App_20251102233002.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import AddEmployee from './Components/AddEmployee'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import ListEmployee from './Components/ListEmployee'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<ListEmployee />} />
+          <Route path='/employees' element={<ListEmployee />} />
+          <Route path='/add-employee' element={<AddEmployee />} />
+          <Route path='/edit-employee/:id' element={<AddEmployee />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
